1e3ccebcc6c842325d3fa51adaddbb44e5110de4
eliserust <eliserust@gmail.com>
2022-05-24 11:38:59 -0400

    initial commit

---
+++ Colorado_BetaTesting.R
@@ -0,0 +1,10 @@
+## Elise Rust
+## MDI
+## Green For All Colorado Beta Testing
+## Exploring districting shapefiles and merging with census variables
+## May 2022
+
+## Load necessary packages
+
+## Load in data
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# MassiveDataInstitute
+# Massive Data Institute
+
 Code for MDI/Green For All climate project aggregating sociodemographic + green tech/entrepreneurship data at congressional district levels
